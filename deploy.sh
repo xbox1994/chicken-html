293709d26b6af8d48fc4dae6e96be33d97640459
WangTianyi <tywang@thoughtworks.com>
2018-01-10 10:17:57 +0800

    change deploy destination

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-scp -o StrictHostKeychecking=no index.html vagrant@192.168.56.101:/tmp/nginx/html
+scp -o StrictHostKeychecking=no index.html -i /var/jenkins_home/nginx_private_key vagrant@192.168.56.102:/tmp/nginx/html
