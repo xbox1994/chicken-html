be0114dcc00756e4f564d211866f7f2f557cfa56
WangTianyi <tywang@thoughtworks.com>
2018-01-18 19:53:30 +0800

    add script note

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+echo '--------------------------start to deploy...--------------------------'
 scp -o StrictHostKeychecking=no -i /var/jenkins_home/nginx_private_key index.html vagrant@192.168.56.102:/data/nginx/html/index.html
+echo '--------------------------deploy finished ! --------------------------'
